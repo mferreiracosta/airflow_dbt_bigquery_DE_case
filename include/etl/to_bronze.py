ea6d641df8a635a52deeb0734002c6f0c0d86153
Matheus Costa <matheeusferreeira@hotmail.com>
2024-10-22 21:16:55 -0300

    feat: add convert_csv_to_parquet_gcs_bronze function and fix local function

--- include/etl/to_bronze.py
+++ include/etl/to_bronze.py
@@ -1,11 +1,11 @@
 import dask.dataframe as dd
 
 
-def convert_csv_to_bronze_parquet(table_name: str, input_path: str, output_path: str, input_format: str = "csv"):
+def convert_csv_to_parquet_local_bronze(filename: str, input_path: str, output_path: str, input_format: str = "csv"):
   
     # Define 'source' and 'destination' path
-    source = f"{input_path}/{table_name}.{input_format}"
-    destination = f"{output_path}/{table_name}"
+    source = f"{input_path}/{filename}/*.{input_format}"
+    destination = f"{output_path}/{filename}"
 
     # Read csv file using dask
     df = dd.read_csv(
@@ -16,8 +16,7 @@ def convert_csv_to_bronze_parquet(table_name: str, input_path: str, output_path:
     )
 
     # Transform 'data_inversa' column to datetime type as 'dd/MM/yyyy'
-    df['data_inversa'] = dd.to_datetime(df['data_inversa'], format='%d/%m/%Y')
-    df['ano'] = df['data_inversa'].dt.year
+    df['ano'] = dd.to_datetime(df['data_inversa'], format='%d/%m/%Y').dt.year
 
     # Load dataframe into parquet using pyarrow engine, snappy compression and
     # partition parquet file by 'ano' and 'uf' columns
@@ -27,5 +26,35 @@ def convert_csv_to_bronze_parquet(table_name: str, input_path: str, output_path:
         write_metadata_file=True,
         compression="snappy",
         name_function=lambda n: f"part-{n}.snappy.parquet",
-        partition_on=["ano", "uf"]
+        partition_on=["ano", "uf"],
+        overwrite=True,
     )
+    print(f"Arquivo CSV {source} convertido e salvo como Parquet em {destination}")
+
+
+def convert_csv_to_parquet_gcs_bronze(filename: str, input_path: str, output_path: str, input_format: str = "csv"):
+    source = f"{input_path}/{filename}/*.{input_format}"
+    destination = f"{output_path}/{filename}"
+
+    # Read csv file using dask
+    df = dd.read_csv(
+        source,
+        sep=";",
+        low_memory=False,
+        dtype="object"
+    )
+
+    # Transform 'data_inversa' column to datetime type as 'dd/MM/yyyy'
+    df['ano'] = dd.to_datetime(df['data_inversa'], format='%d/%m/%Y').dt.year
+
+    # Load dataframe into parquet using pyarrow engine, snappy compression and
+    # partition parquet file by 'ano' and 'uf' columns
+    df.to_parquet(
+        destination,
+        engine="pyarrow",
+        write_metadata_file=False,
+        compression="snappy",
+        name_function=lambda n: f"part-{n}.snappy.parquet",
+        overwrite=True,
+    )
+    print(f"Arquivo CSV {source} convertido e salvo como Parquet em {destination}")
